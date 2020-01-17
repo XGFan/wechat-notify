5b5fd1375130e9fe5d6bb53b076654f6b6469f80
Guofan Xu <xuguofan@live.com>
2020-01-17 16:55:38 +0800

    update

--- src/main/kotlin/com/test4x/app/notify/NotifyApplication.kt
+++ src/main/kotlin/com/test4x/app/notify/NotifyApplication.kt
@@ -5,9 +5,9 @@ import io.lettuce.core.RedisURI
 import me.chanjar.weixin.cp.api.impl.WxCpServiceImpl
 import me.chanjar.weixin.cp.bean.WxCpMessage
 import me.chanjar.weixin.cp.config.WxCpInMemoryConfigStorage
-import org.apache.commons.lang3.RandomStringUtils
 import org.jtwig.JtwigModel
 import org.jtwig.JtwigTemplate
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import spark.Filter
 import spark.Spark
@@ -15,22 +15,19 @@ import java.io.File
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
+class NotifyApplication(private val config: Config) {
 
-fun main(args: Array<String>) {
-    val logger = LoggerFactory.getLogger("notify")
+    private val logger: Logger = LoggerFactory.getLogger("notify")
 
-    val config = Config(File("application.properties"))
+    private val wxCpService = WxCpServiceImpl().apply {
+        wxCpConfigStorage = WxCpInMemoryConfigStorage().apply {
+            corpId = config.get("corpId")
+            agentId = config.get("agentId")?.toInt()
+            corpSecret = config.get("agentSecret")
+        }
+    }
 
-    val wxCpConfigStorage = WxCpInMemoryConfigStorage()
-    wxCpConfigStorage.corpId = config.get("corpId")
-    wxCpConfigStorage.agentId = config.get("agentId")?.toInt()
-    wxCpConfigStorage.corpSecret = config.get("agentSecret")
-
-    val wxCpService = WxCpServiceImpl()
-    wxCpService.wxCpConfigStorage = wxCpConfigStorage
-
-    val redisMode = (config.get("redis.enable") ?: "false").toBoolean()
-    val repo = if (redisMode) {
+    private val repo = if ((config.get("redis.enable") ?: "false").toBoolean()) {
         val host = config.get("redis.host") ?: "127.0.0.1"
         val port = config.get("redis.port")?.toInt() ?: 6379
         val pwd = config.get("redis.password")
@@ -51,8 +48,8 @@ fun main(args: Array<String>) {
             override fun get(key: String): String? = sync[key]
         }
     } else {
-        val map = ConcurrentHashMap<String, String>()
         object : Repo {
+            val map = ConcurrentHashMap<String, String>()
             override fun put(key: String, value: String) {
                 map[key] = value
             }
@@ -61,27 +58,54 @@ fun main(args: Array<String>) {
         }
     }
 
-    val baseUrl = config.get("baseUrl")
+    private val baseUrl = config.get("baseUrl")
 
-    val detailView = JtwigTemplate.classpathTemplate("templates/wechat.twig")
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            NotifyApplication(Config(File("application.properties"))).run()
+        }
+    }
 
-    Spark.port(config.get("server.port")?.toInt() ?: 8080)
-    Spark.after(Filter { req, res ->
-        res.header("Content-Encoding", "gzip")
-    })
+    fun run() {
+        val detailView = JtwigTemplate.classpathTemplate("templates/wechat.twig")
+        Spark.port(config.get("server.port")?.toInt() ?: 8080)
+        Spark.after(Filter { _, res ->
+            res.header("Content-Encoding", "gzip")
+        })
 
-    Spark.post("wechat") { req, res ->
-        val title = req.queryParams("title") ?: return@post "fail"
-        val content = req.queryParams("content") ?: return@post "fail"
-        val user = req.queryParams("user") ?: return@post "fail"
-        val randomId = RandomStringUtils.randomAlphanumeric(16)
+        Spark.get("wechat/:randomId") { req, res ->
+            val randomId = req.params(":randomId")
+            val title = repo.get("$randomId|title") ?: return@get "Error"
+            val content = repo.get("$randomId|content") ?: return@get "Error"
+            val model = JtwigModel.newModel().with("title", title).with("content", content)
+            detailView.render(model)
+        }
+
+        Spark.post("wechat") { req, res ->
+            val title = req.queryParams("title") ?: return@post "fail"
+            val content = req.queryParams("content") ?: return@post "fail"
+            val toUser = req.queryParams("user") ?: return@post "fail"
+            sentWechat(title, content, toUser)
+        }
+
+        Spark.post("mail2wechat") { req, res ->
+            val title = req.queryParams("subject")
+            val content = req.queryParams("body-plain")
+            val toUser = req.queryParams("recipient").split("@")[0]
+            sentWechat(title, content, toUser)
+        }
+    }
+
+    private fun sentWechat(title: String, content: String, user: String): String {
+        val randomId = randomString(16)
         val message = WxCpMessage.TEXTCARD()
-                .title(title)
-                .description(content)
-                .toUser(user)
-                .url("$baseUrl/wechat/$randomId")
-                .build()
-        if (wxCpService.messageSend(message).errCode == 0) {
+            .title(title)
+            .description(content)
+            .toUser(user)
+            .url("$baseUrl/wechat/$randomId")
+            .build()
+        return if (wxCpService.messageSend(message).errCode == 0) {
             repo.put("$randomId|title", title)
             repo.put("$randomId|content", content)
             logger.info("{}|{}|{}", randomId, title, content)
@@ -90,81 +114,35 @@ fun main(args: Array<String>) {
             "fail"
         }
     }
-    Spark.get("wechat/:randomId") { req, res ->
-        val randomId = req.params(":randomId")
-        val title = repo.get("$randomId|title") ?: return@get "Error"
-        val content = repo.get("$randomId|content") ?: return@get "Error"
-        val model = JtwigModel.newModel().with("title", title).with("content", content)
-        detailView.render(model)
-    }
 
-    Spark.post("mail2wechat") { req, res ->
-        val title = req.queryParams("subject")
-        val content = req.queryParams("body-plain")
-        val toUser = req.queryParams("recipient").split("@")[0]
-        val randomId = RandomStringUtils.randomAlphanumeric(16)
-        val message = WxCpMessage.TEXTCARD()
-                .title(title)
-                .description(content)
-                .toUser(toUser)
-                .url("$baseUrl/wechat/$randomId")
-                .build()
-        if (wxCpService.messageSend(message).errCode == 0) {
-            repo.put("$randomId|title", title)
-            repo.put("$randomId|content", content)
-            logger.info("{}|{}|{}", randomId, title, content)
-            "ok"
-        } else {
-            "fail"
+    private fun randomString(len: Int): String {
+        val list = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        val random = Random()
+        val stringBuilder = StringBuilder()
+        repeat(len) {
+            val i = random.nextInt(list.size)
+            stringBuilder.append(list[i])
         }
+        return stringBuilder.toString()
     }
-}
 
-fun randomString(len: Int): String {
-    val random = Random()
-    val stringBuilder = StringBuilder()
-    repeat(len) {
-        val i = random.nextInt(62)
-        val x = when {
-            i < 10 -> (i + 48).toChar()
-            i < 36 -> (i + 55).toChar()
-            else -> (i + 61).toChar()
+    class Config(file: File) {
+        private val properties: Properties = Properties()
+
+        fun get(key: String): String? {
+            return properties[key]?.toString() ?: System.getenv(key)
         }
-        stringBuilder.append(x)
-    }
-    return stringBuilder.toString()
-}
 
-fun randomNumString(len: Int): String {
-    val random = Random()
-    val stringBuilder = StringBuilder()
-    repeat(len) {
-        val i = random.nextInt(10)
-        stringBuilder.append(i + 48)
-    }
-    return stringBuilder.toString()
-}
-
-class Config {
-    val properties: Properties = Properties()
-
-    fun get(key: String): String? {
-        return properties[key]?.toString() ?: System.getenv(key)
-    }
-
-    constructor(file: File) {
-        if (!file.exists()) {
-            //
-        } else {
-            properties.load(file.inputStream())
+        init {
+            if (file.exists()) {
+                properties.load(file.inputStream())
+            }
         }
     }
 
+    interface Repo {
+        fun put(key: String, value: String)
 
-}
-
-interface Repo {
-    fun put(key: String, value: String)
-
-    fun get(key: String): String?
+        fun get(key: String): String?
+    }
 }
